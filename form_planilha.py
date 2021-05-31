599b8dd7d5b017bbb92f0e225fd4624da5643e18
Alexandre Baiocco <83550178+Alebaiocco@users.noreply.github.com>
2021-05-30 22:09:14 -0300

    Update form_planilha.py

--- form_planilha.py
+++ form_planilha.py
@@ -1,10 +1,10 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
 arquivo_excel = Workbook()
-wb = load_workbook('X.xlsx') #arquivo .xlsx
-ws = wb['Folha1'] #página
+wb = load_workbook('X.xlsx') #document .xlsx
+ws = wb['Folha1'] #page 
 for line in ws:    
-	line[1].value = (str (line[1].value)).replace("+55","").replace("-", "").replace("(","").replace(")","").replace(" ","").replace(".","") #linha e oq deseja remover 
+	line[1].value = (str (line[1].value)).replace("+55","").replace("-", "").replace("(","").replace(")","").replace(" ","").replace(".","") #line and what you want remove 
 
 	print (line[1].value)
 wb.save(filename = 'Teste.xlsx')
